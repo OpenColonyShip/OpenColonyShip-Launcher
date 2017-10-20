d91c881020dfc5637763ec571cbd990b870b25f1
Microlith57 <microlith57@gmail.com>
2017-10-21 07:26:51 +1300

    Add a little test to see if this'll work on Travis

---
+++ test/all.js
@@ -0,0 +1,7 @@
+// using assert passed to the test function that just logs failures
+exports['test that logs all failures'] = function(assert) {
+  assert.equal(2 + 2, 5, '2 + 2 == 5')
+  assert.equal(3 + 2, 5, '3 + 2 == 5')
+}
+
+if (module == require.main) require('test').run(exports)
