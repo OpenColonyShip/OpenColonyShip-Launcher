ece36bed65e1afd9e836a6774d957501616b87b3
Microlith57 <microlith57@gmail.com>
2017-10-21 08:23:14 +1300

    Make it succeed

--- test/all.js
+++ test/all.js
@@ -1,6 +1,6 @@
 // using assert passed to the test function that just logs failures
 exports['test that logs all failures'] = function(assert) {
-  assert.equal(2 + 2, 5, '2 + 2 == 5')
+  //assert.equal(2 + 2, 5, '2 + 2 == 5')
   assert.equal(3 + 2, 5, '3 + 2 == 5')
   assert.equal(5 + 5, 10,'5 + 5 == 10')
 }
